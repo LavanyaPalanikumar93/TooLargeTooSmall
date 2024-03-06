714edf426349da3ce9e3444d744e533fa1def40e
LavanyaPalanikumar93 <lavanyasaii93@gmail.com>
2024-03-06 15:25:45 -0500

    committed

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,32 @@
+import java.util.Random;
+import java.util.Scanner;
 /**
  * Created by iyasuwatts on 10/17/17.
  */
 public class Main {
+            public static void main(String[] args) {
+                Scanner scanner = new Scanner(System.in);
+                System.out.println("Welcome to Guessing Game");
+                System.out.println("Enter your Guess");
+                Random random = new Random();
+                int numberToguess = random.nextInt(10);
+                boolean guessedcorrectly = false;
+                int numberOftries = 0;
+                {
+                    while (!guessedcorrectly) {
+                        int guess = scanner.nextInt();
+                        numberOftries++;
+                        if (guess > numberToguess) {
+                            System.out.println("Guess is TooLarge try again");
+                        } else if (guess < numberToguess) {
+                            System.out.println("Guess is TooSmall");
+                        } else {
+                            guessedcorrectly = true;
+                        }
+                    }
+                }
+                System.out.println("congratulations!!!you guessed correctly");
+            }
+        }
 
-    public static void main(String[] args){
-        
-    }
-}
+
